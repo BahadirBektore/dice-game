49ac7e838d6da625f353c40cc9a14e281874e9c7
Bahadır <bahadirbektore@gmail.com>
2026-03-26 23:23:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll(".img1")[0].setAttribute("src", randomDiceImage);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll(".img2")[0].setAttribute("src", randomDiceImage);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
